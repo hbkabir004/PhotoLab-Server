8d8904b4e7664b1d59130ef972084a1517be87bc
ABIR <hbkabir004@gmail.com>
2022-10-24 21:34:13 +0600

    cors added

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require('express')
+const app = express()
+const port = 5000
+const cors = require('cors')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
